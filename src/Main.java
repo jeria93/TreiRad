946b842669ccd63ce0f581f96b178160dd013595
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-10-04 14:35:09 +0200

    Trying to use 2D arrays for representation of the board of the game

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-                String [][] cars = new String[3][3];
 
-        String [][] cars2 = {
-                {"BMW", "Mercedes", "Audi"},
-                {"Volvo", "Ford", "Mazda"},
-                {"Toyota", "Honda", "Ford"}
+        //        Represents our ticktacktoe board. 2D array, helps to understand positioning.
+        char[][] board = {
 
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
         };
 
-//        Row 0, --->
-        cars[0][0] = "BMW";
-        cars[0][1] = "Mercedes";
-        cars[0][2] = "Audi";
-
-//        Row 1, colum
-        cars[1][0] = "Volvo";
-        cars[1][1] = "Ford";
-        cars[1][2] = "Mazda";
-
-//        Row 2, column
-        cars[2][0] = "Toyota";
-        cars[2][1] = "Honda";
-        cars[2][2] = "Ford";
-
-//        Skriver ut allt, alla rader, alla kolumner
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println();
-            for (int j = 0; j < cars[i].length; j++) {
-                System.out.print(cars[i][j] + " ");
-            }
-
-        }
-        System.out.println();
-        System.out.println(cars[2][0]); //Toyota
 
     }
 }
